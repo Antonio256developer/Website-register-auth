5fc6f02633c02994d6cc855c8e816fc9ade4f3ff
Anton <65733048+Antonio256developer@users.noreply.github.com>
2020-10-13 16:02:15 +0300

    Update auth.service.ts

--- front-end-app/src/app/auth.service.ts
+++ front-end-app/src/app/auth.service.ts
@@ -1,6 +1,5 @@
 import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
-//import 'rxjs/add/operator/map';
 import { map } from 'rxjs/operators';
 import { tokenNotExpired } from 'angular2-jwt';
 
@@ -13,23 +12,23 @@ export class AuthService {
   user: any;
 
   constructor(private http: Http) { }
-//регистрация пользователя в БД
-registerUser(user) {   //ф-ция принимает объект(все данные что ввел пользователь в форму)
-  let headers = new Headers();    //ф-ция отправляет объект по url адресу
+
+registerUser(user) {   
+  let headers = new Headers();    
   headers.append('Content-Type', 'application/json');
   return this.http.post('account/reg', user,
   {headers: headers}).pipe(map((respone: any) => respone.json()));
 }
-   //передача объекта user по 'http://localhost:3000/account/auth'
+   
 authUser(user) {
-  let headers = new Headers();    //ф-ция отправляет объект по url адресу
+  let headers = new Headers();    
   headers.append('Content-Type','application/json');
   return this.http.post('account/auth', user,
   {headers: headers}).pipe(map((respone: any) => respone.json()));
 }
 
 storeUser(token, user) {
-  localStorage.setItem('token', token); //локадьное хранилище
+  localStorage.setItem('token', token); 
   localStorage.setItem('user', JSON.stringify(user));
   this.token = token;
   this.user = user;
